ec5994e25f3e72e344bce48e689127928f23593d
nogw <gabrielnogueiraoliveira@gmail.com>
2021-12-28 16:36:48 -0300

    ring server

--- project.clj
+++ project.clj
@@ -3,15 +3,16 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [
-                  [org.clojure/clojure "1.10.0"]
-                  [com.taoensso/carmine "3.1.0"]
-                  [ring/ring-core "1.7.1"]
-                  [ring/ring-jetty-adapter "1.7.1"]
-                  [ring-cors "0.1.8"]
-                  [ring-transit "0.1.6"]
-                  [compojure "1.6.2"]
-                ]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [cheshire "5.10.1"]
+                 [com.taoensso/carmine "3.1.0"]
+                 [ring/ring-core "1.7.1"]
+                 [ring/ring-jetty-adapter "1.7.1"]
+                 [ring/ring-json "0.5.1"]
+                 [ring-cors "0.1.8"]
+                 [ring-transit "0.1.6"]
+                 [compojure "1.6.2"]]
   :main ^:skip-aot clojure-redis-boilerplate.core
   :target-path "target/%s"
+  :plugins [[lein-cljfmt "0.8.0"]]
   :profiles {:uberjar {:aot :all}})
--- src/clojure_redis_boilerplate/core.clj
+++ src/clojure_redis_boilerplate/core.clj
@@ -1,58 +1,75 @@
 (ns clojure-redis-boilerplate.core
-  (:require 
-    [taoensso.carmine :as carmine :refer [wcar]]
-    [compojure.core :refer [context defroutes GET]]
-    [compojure.handler :as handler]
-    [compojure.route :as route]
-    [ring.adapter.jetty :as jetty]
-    [ring.middleware.cors :refer [wrap-cors]]
-    [ring.middleware.transit :as middleware]
-    [ring.util.response :refer [redirect response]]
-  )
+  (:require
+   [taoensso.carmine :as car :refer [wcar]]
+   [compojure.core :refer [context defroutes GET POST DELETE]]
+   [compojure.handler :as handler]
+   [compojure.route :as route]
+   [ring.adapter.jetty :as jetty]
+   [ring.middleware.cors :refer [wrap-cors]]
+   [ring.middleware.transit :as middleware]
+   [ring.middleware.json :refer [wrap-json-body]]
+   [ring.util.response :as resp :refer [response]]
+   [cheshire.core :as json])
   (:gen-class))
 
 (defonce redis-connection {:pool {} :spec {:spec (System/getenv "REDIS_URL")}})
 
 (defn set-key
   [key data]
-  (wcar redis-connection (carmine/set key data)))
+  (wcar redis-connection (car/set key data)))
 
 (defn get-key
   [key]
-  (wcar redis-connection (carmine/get key)))
+  (wcar redis-connection (car/get key)))
 
 (defn del-key
   [key]
-  (wcar redis-connection (carmine/del key)))
+  (wcar redis-connection (car/del key)))
 
-(defn wrap-response 
+(defn wrap-response
   [res & more]
-  (-> (apply merge {:data res} more)
-  (response)))
+  (->
+   (apply merge {:data res} more)
+   (json/generate-string)
+   (response)))
 
-(defn get-key-route []
-  (let [x (get-key "name")]
-  (wrap-response x))
-)
+(defn set-key-route
+  [req]
+  (println (:body req))
+  (let [j (:body req)]
+    (set-key (j :key) (j :value))
+    (wrap-response (:body req))))
+
+(defn get-key-route
+  [key]
+  (let [x (get-key key)]
+    (wrap-response {:key key :value x})))
+
+(defn del-key-route
+  [key]
+  (del-key key)
+  (wrap-response "deleted"))
 
 (defroutes key-routes
-  (GET "/" [] (get-key-route))
+  (GET "/:key" [key] (get-key-route key))
+  (POST "/" [] (wrap-json-body set-key-route {:keywords? true :bigdecimals? true}))
+  (DELETE "/:key" [key] (del-key-route key))
   (route/not-found "Not found"))
 
 (defroutes app-routes
-  (context "/key" [] key-routes)
+  (context "/" [] key-routes)
   (route/not-found "Not found"))
 
 (def app
-  ( -> 
-    (handler/api app-routes)
-    (middleware/wrap-transit-response {:encoding :json :opts {}})
-    (wrap-cors :access-control-allow-origin #".*localhost.*"
-               :access-control-allow-methods [:get]
-               :access-control-allow-headers ["Origin" "X-Requested-With"
-                                              "Content-Type" "Accept"
-                                              "Cache-Control" "Accept-Encoding"])))
+  (->
+   (handler/api app-routes)
+   (middleware/wrap-transit-response {:encoding :json :opts {}})
+   (wrap-cors :access-control-allow-origin #".*localhost.*"
+              :access-control-allow-methods [:get]
+              :access-control-allow-headers ["Origin" "X-Requested-With"
+                                             "Content-Type" "Accept"
+                                             "Cache-Control" "Accept-Encoding"])))
 
 (defn -main
-  [& args]
-  (jetty/run-jetty #'app {:port 3001 :join? false}))
+  [& _]
+  (jetty/run-jetty #'app {:port 3013 :join? false}))
